490bee91a9a26b2f4d3229a84a460177d51ef149
Helen <paznachka@gmail.com>
2020-12-16 13:08:40 +0200

    edited app.js

--- app.js
+++ app.js
@@ -8,32 +8,28 @@ for (let i = 0; i < addToCartBtns.length; i++) {
 }
 
 let favoriteEl = document.getElementsByClassName("favorite-icon");
-
 for (let i=0; i<favoriteEl.length; i++) {
     favoriteEl[i].addEventListener("click", function() {
-        if (favoriteEl[i].style.background = "url(images/favorite-blue.png)") {
+        if(favoriteEl[i].classList.contains("on")) {
+            favoriteEl[i].style.background = "url(images/favorite-blue.png)";
+            favoriteEl[i].style.backgroundRepeat = "no-repeat";
+            favoriteEl[i].style.backgroundPosition = "center";
+            favoriteEl[i].style.backgroundColor = "transparent";
+            favoriteEl[i].classList.remove("on");
+        } else {
             favoriteEl[i].style.background = "url(images/favorite-white.png)";
             favoriteEl[i].style.backgroundRepeat = "no-repeat";
             favoriteEl[i].style.backgroundPosition = "center";
             favoriteEl[i].style.backgroundColor = "rgb(44, 113, 184)";
-            // favoriteEl[i].classList.add("on");
+            favoriteEl[i].classList.add("on");
         }
     })
 }
 
-// let favoriteElOn = document.getElementsByClassName("on"); 
 
-// for (let i=0; i<favoriteElOn.length; i++) {
-//     favoriteElOn[i].addEventListener("click", function() {
-        // if (favoriteEl[i].style.background = "url(images/favorite-white.png)") {
-        //     favoriteElOn[i].style.background = "url(images/favorite-blue.png)";
-        //     favoriteElOn[i].style.backgroundRepeat = "no-repeat";
-        //     favoriteElOn[i].style.backgroundPosition = "center";
-        //     favoriteElOn[i].style.backgroundColor = "transparent";
-        // } 
-//     })
+// if (favoriteEl[i].style.background == "url(images/favorite-white.png)") {
+    // }
 
-// }
 
 
 
